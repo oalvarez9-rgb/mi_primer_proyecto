99b14164b153d74c03d3ba19e71f865585c5a0aa
oalvarez9-rgb <Oalvarez9@ucol.mx>
2026-09-06 17:02:31 -0600

    mi primer proyecto

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("===============================\n");
+    printf("         ¡Hola, GITHUB!\n");
+    printf("==============================\n\n");
+    printf("Mi nombre es: Owen Alvarez\n");
+    printf("Estoy aprendiendo programacion..\n");
+    printf("Este es mi primer programa\n");
+    printf("Publicado en Github\n\n");
+    printf("¡Bienvenido a mi repositorio!\n");
+    printf("¡Adios!\n");
+    return 0;
+}
